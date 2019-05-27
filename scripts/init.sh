0bb4ab20499d55ebff6054f828535bc1c288903d
Marcel Frei <marcel.frei@electronix.ch>
2019-05-27 17:20:03 +0200

    init.sh: use nightly-2019-05-21

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,8 +9,8 @@ if [ -z $CI_PROJECT_NAME ] ; then
    rustup update stable
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly-2019-05-21
 
 # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
 command -v wasm-gc || \
-	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
+	cargo +nightly-2019-05-21 install --git https://github.com/alexcrichton/wasm-gc --force

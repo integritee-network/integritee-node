2d19d9920af48b0475915a18a4145dd2a17eb991
Marcel Frei <marcel.frei@electronix.ch>
2019-09-27 09:15:32 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,21 @@ mod chain_spec;
 mod service;
 mod cli;
 
-pub use substrate_cli::{VersionInfo, IntoExit, error};
+pub use substrate_cli::{error, IntoExit, VersionInfo};
 
 fn main() {
-	let version = VersionInfo {
-		name: "Substrate Node",
-		commit: env!("VERGEN_SHA_SHORT"),
-		version: env!("CARGO_PKG_VERSION"),
-		executable_name: "substratee-node",
-		author: "Supercomputing Systems AG",
-		description: "substratee-node",
-		support_url: "support.anonymous.an",
-	};
+    let version = VersionInfo {
+        name: "Substrate Node",
+        commit: env!("VERGEN_SHA_SHORT"),
+        version: env!("CARGO_PKG_VERSION"),
+        executable_name: "substratee-node",
+        author: "Supercomputing Systems AG",
+        description: "substratee-node",
+        support_url: "support.anonymous.an",
+    };
 
-	if let Err(e) = cli::run(::std::env::args(), cli::Exit, version) {
-		eprintln!("Fatal error: {}\n\n{:?}", e, e);
-		std::process::exit(1)
-	}
+    if let Err(e) = cli::run(::std::env::args(), cli::Exit, version) {
+        eprintln!("Fatal error: {}\n\n{:?}", e, e);
+        std::process::exit(1)
+    }
 }

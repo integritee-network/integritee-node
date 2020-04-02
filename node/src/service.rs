991042cab9052c14fbc89959becc6c1aae995b10
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2020-04-02 13:31:26 +0200

    fixes for v0.2.0

--- node/src/service.rs
+++ node/src/service.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 use std::time::Duration;
 use sc_client_api::ExecutorProvider;
 use sc_consensus::LongestChain;
-use node_template_runtime::{self, opaque::Block, RuntimeApi};
+use encointer_node_runtime::{self, opaque::Block, RuntimeApi};
 use sc_service::{error::{Error as ServiceError}, AbstractService, Configuration, ServiceBuilder};
 use sp_inherents::InherentDataProviders;
 use sc_executor::native_executor_instance;
@@ -17,8 +17,8 @@ use sc_finality_grandpa::{
 // Our native executor instance.
 native_executor_instance!(
 	pub Executor,
-	node_template_runtime::api::dispatch,
-	node_template_runtime::native_version,
+	encointer_node_runtime::api::dispatch,
+	encointer_node_runtime::native_version,
 );
 
 /// Starts a `ServiceBuilder` for a full service.
@@ -34,7 +34,7 @@ macro_rules! new_full_start {
 		let inherent_data_providers = sp_inherents::InherentDataProviders::new();
 
 		let builder = sc_service::ServiceBuilder::new_full::<
-			node_template_runtime::opaque::Block, node_template_runtime::RuntimeApi, crate::service::Executor
+			encointer_node_runtime::opaque::Block, encointer_node_runtime::RuntimeApi, crate::service::Executor
 		>($config)?
 			.with_select_chain(|_config, backend| {
 				Ok(sc_consensus::LongestChain::new(backend.clone()))

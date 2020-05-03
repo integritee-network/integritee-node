52b5fd13167ce11cd1be7040023f0c6f086ff926
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2020-05-03 11:53:07 +0200

    bump version 0.6.1

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -98,8 +98,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("substratee-node-runtime"),
 	impl_name: create_runtime_str!("substratee-node-runtime"),
 	authoring_version: 1,
-	spec_version: 1,
-	impl_version: 2,
+	spec_version: 2,
+	impl_version: 3,
 	apis: RUNTIME_API_VERSIONS,
 };
 

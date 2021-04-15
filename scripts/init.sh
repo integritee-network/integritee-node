977a16e55987e792f3fd8de37c1018ebc791b32a
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2021-04-15 10:16:44 +0200

    add toolchain var to init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,9 +4,11 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
+TOOLCHAIN=$(cat ./rust-toolchain)
+
 if [ -z $CI_PROJECT_NAME ] ; then
    rustup update nightly
    rustup update stable
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup target add wasm32-unknown-unknown --toolchain $TOOLCHAIN

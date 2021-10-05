c9ebd2176bad3b0774961fc8e9de2baee8659ba7
clangenb <37865735+clangenb@users.noreply.github.com>
2021-10-05 07:18:35 +0200

    Benchmark all pallets (#58)
    * add bennchmark scripts and run benchmarks
    
    * [GA] update chain-spec name in benchmark scripts
    
    * add pallet-scheduler to benchmarks and re-run benchmarks
    
    * update frame_system weights
    
    * run rustfmt

---
+++ runtime/src/weights/mod.rs
@@ -0,0 +1,24 @@
+//! The weights used in the runtime
+//!
+//! The current weights have been obtained with the following reference hardware:
+//! * Core(TM) i7-10875H
+//! * 32GB of RAM
+//! * NVMe SSD
+
+// the generated files to not pass clippy
+#![allow(clippy::all)]
+
+// note: Always double check the frame_system generated weights. Sometimes some values are 0, but
+// return ridiculously high weights in another run. When re-running the benchmarks always perform
+// sanity checks with substrate's weight for this module. The close to 0 values seem to be correct.
+//
+// Problematic functions are: `remark`, `remark_with_event`.
+pub mod frame_system;
+pub mod pallet_balances;
+pub mod pallet_multisig;
+pub mod pallet_proxy;
+pub mod pallet_scheduler;
+pub mod pallet_teerex;
+pub mod pallet_timestamp;
+pub mod pallet_treasury;
+pub mod pallet_vesting;

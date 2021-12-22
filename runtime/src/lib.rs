9603917b9ce71614845d28f2fe4972550f9ab900
Alain Brenzikofer <news@brenzi.ch>
2021-12-22 14:56:21 +0100

    bump version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -126,7 +126,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	/// Version of the runtime specification. A full-node will not attempt to use its native
 	/// runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	/// `spec_version` and `authoring_version` are the same between Wasm and native.
-	spec_version: 5,
+	spec_version: 6,
 
 	/// Version of the implementation of the specification. Nodes are free to ignore this; it
 	/// serves only as an indication that the code is different; as long as the other two versions
@@ -147,7 +147,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	/// index.
 	///
 	/// It need *not* change when a new module is added or when a dispatchable is added.
-	transaction_version: 1,
+	transaction_version: 2,
 };
 
 /// This determines the average expected block time that we are targeting.

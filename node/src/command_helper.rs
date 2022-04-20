f8f1b0e8de523cee6851c002f8dd528771a99f1d
echevrier <edith.chevrier@scs.ch>
2022-04-20 17:28:31 +0200

    Add comments

--- node/src/command_helper.rs
+++ node/src/command_helper.rs
@@ -79,6 +79,7 @@ pub fn create_benchmark_extrinsic(
 		.map(|c| c / 2)
 		.unwrap_or(2) as u64;
 	let extra: runtime::SignedExtra = (
+		// TODO: Integrate upstream-changes after scs/substrate-api-client#211 has been solved.
 		//frame_system::CheckNonZeroSender::<runtime::Runtime>::new(),
 		frame_system::CheckSpecVersion::<runtime::Runtime>::new(),
 		frame_system::CheckTxVersion::<runtime::Runtime>::new(),
@@ -96,7 +97,8 @@ pub fn create_benchmark_extrinsic(
 		call.clone(),
 		extra.clone(),
 		(
-			//	(),
+			// TODO: Integrate upstream-changes after scs/substrate-api-client#211 has been solved.
+			// 	(),
 			runtime::VERSION.spec_version,
 			runtime::VERSION.transaction_version,
 			genesis_hash,

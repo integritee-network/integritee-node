4ba05d589751fabafb375d8abe41fa552d58b39f
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-05-14 18:29:37 +0300

    [runtime] remove unused `add_benchmark` import

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -913,7 +913,7 @@ impl_runtime_apis! {
 		fn dispatch_benchmark(
 			config: frame_benchmarking::BenchmarkConfig
 		) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, sp_runtime::RuntimeString> {
-			use frame_benchmarking::{Benchmarking, BenchmarkBatch, add_benchmark, TrackedStorageKey};
+			use frame_benchmarking::{Benchmarking, BenchmarkBatch, TrackedStorageKey};
 
 			use frame_system_benchmarking::Pallet as SystemBench;
 			impl frame_system_benchmarking::Config for Runtime {}

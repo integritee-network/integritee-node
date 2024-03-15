c8a5725f717ba8598a7b1390b5c1702da7c31dbf
brenzi <brenzi@users.noreply.github.com>
2024-03-15 12:46:23 +0100

    SBliFF backport for CI of worker sdk >=0.12.12 (#199)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -141,7 +141,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	/// Version of the runtime specification. A full-node will not attempt to use its native
 	/// runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	/// `spec_version` and `authoring_version` are the same between Wasm and native.
-	spec_version: 37,
+	spec_version: 38,
 
 	/// Version of the implementation of the specification. Nodes are free to ignore this; it
 	/// serves only as an indication that the code is different; as long as the other two versions
@@ -162,7 +162,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	/// index.
 	///
 	/// It need *not* change when a new module is added or when a dispatchable is added.
-	transaction_version: 6,
+	transaction_version: 7,
 	state_version: 0,
 };
 

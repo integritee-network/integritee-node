f5a4fd0cfea1fef82d7cfe32f9091e85003b6071
Alain Brenzikofer <alain@integritee.network>
2024-07-15 17:01:36 +0200

    bump versions

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -117,7 +117,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("integritee-solo"),
 	impl_name: create_runtime_str!("integritee-solo"),
 	authoring_version: 1,
-	spec_version: 390,
+	spec_version: 400,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 8,
